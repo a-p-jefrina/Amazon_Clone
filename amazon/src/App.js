a1acd6779aa7b6cc4b1a9cfae85630a9b2fa94c5
Jefrina <apjefrina123@gmail.com>
2025-01-10 23:14:28 +0530

    User Auth finished

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -4,7 +4,35 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
+import { useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import firebaseApp from "./firebase";
+import { useStateValue } from "./StateProvider";
+
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    //like dynamic if statement used for storing sign-in
+    //[]-will only run once when the app component loads...
+    //[user,basket] every time user / basket changes it loads
+    const auth = getAuth(firebaseApp);
+    onAuthStateChanged(auth, (authUser) => {
+      console.log("THE USER IS >>>> ", authUser);
+      if (authUser) {
+        //user just logged in or was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        //user logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
     // BEM
     <Router>
--- amazon/src/Header.js
+++ amazon/src/Header.js
@@ -4,10 +4,17 @@ import SearchIcon from "@mui/icons-material/Search";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
+import { getAuth, signOut } from "firebase/auth";
+import firebaseApp from "./firebase";
 
 function Header() {
-  const [{ basket }, dispatch] = useStateValue();
-
+  const [{ basket, user }, dispatch] = useStateValue();
+  const handleAuthentication = () => {
+    if (user) {
+      const auth = getAuth(firebaseApp);
+      signOut(auth);
+    }
+  };
   return (
     <div className="header">
       <Link to="/">
@@ -22,27 +29,31 @@ function Header() {
         <SearchIcon className="header__searchIcon" />
       </div>
       <div className="header__nav">
-        <Link to="/login">
-          <div className="header__option">
-            <spam className="header__optionLineOne">Hello Guest</spam>
-            <spam className="header__optionLineTwo">Sign In</spam>
+        <Link to={!user && "/login"}>
+          <div onClick={handleAuthentication} className="header__option">
+            <span className="header__optionLineOne">
+              Hello {!user ? "Guest" : user?.email}
+            </span>
+            <span className="header__optionLineTwo">
+              {user ? "Sign Out" : "Sign In"}
+            </span>
           </div>
         </Link>
         <div className="header__option">
-          <spam className="header__optionLineOne">Returns</spam>
-          <spam className="header__optionLineTwo">& Orders</spam>
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
         </div>
         <div className="header__option">
-          <spam className="header__optionLineOne">Your</spam>
-          <spam className="header__optionLineTwo">Prime</spam>
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
         </div>
         <Link to="/checkout">
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
-            <spam className="header__optionLineTwo header__basketCount">
+            <span className="header__optionLineTwo header__basketCount">
               {basket?.length}
               {/*? to handle error if something happen*/}
-            </spam>
+            </span>
           </div>
         </Link>
       </div>
